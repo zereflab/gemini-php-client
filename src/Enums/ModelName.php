1df629d4c2d6c914a9311fcc85bf2992c70a0b0c
Syed Akash <36274335+Akash4686@users.noreply.github.com>
2024-06-22 00:37:43 +0400

    Update ModelName.php

--- src/Enums/ModelName.php
+++ src/Enums/ModelName.php
@@ -8,6 +8,7 @@ enum ModelName: string
 {
     case Default = 'models/text-bison-001';
     case GeminiPro = 'models/gemini-pro';
+    case GeminiFlash = 'models/gemini-1.5-flash';
     case GeminiProVision = 'models/gemini-pro-vision';
     case Embedding = 'models/embedding-001';
     case AQA = 'models/aqa';
